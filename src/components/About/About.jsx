7245bc88e5d678795b0ee3d958969ac7e37d509d
kwsantiago <kwsantiago@usf.edu>
2020-11-13 18:57:04 -0500

    Init

---
+++ src/components/About/About.jsx
@@ -0,0 +1,69 @@
+import React, { useContext, useState, useEffect } from 'react';
+import Fade from 'react-reveal/Fade';
+import { Container, Row, Col } from 'react-bootstrap';
+import Title from '../Title/Title';
+import AboutImg from '../Image/AboutImg';
+import PortfolioContext from '../../context/context';
+
+const About = () => {
+  const { about } = useContext(PortfolioContext);
+  const { img, paragraphOne, paragraphTwo, paragraphThree, resume } = about;
+
+  const [isDesktop, setIsDesktop] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth > 769) {
+      setIsDesktop(true);
+      setIsMobile(false);
+    } else {
+      setIsMobile(true);
+      setIsDesktop(false);
+    }
+  }, []);
+
+  return (
+    <section id="about">
+      <Container>
+        <Title title="About Me" />
+        <Row className="about-wrapper">
+          <Col md={6} sm={12}>
+            <Fade bottom duration={1000} delay={600} distance="30px">
+              <div className="about-wrapper__image">
+                <AboutImg alt="profile picture" filename={img} />
+              </div>
+            </Fade>
+          </Col>
+          <Col md={6} sm={12}>
+            <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
+              <div className="about-wrapper__info">
+                <p className="about-wrapper__info-text">
+                  {paragraphOne ||
+                    "Kyle is a blockchain engineer and researcher currently focused on developing smart contracts and managing blockchain Nodes as a Service (NaaS) platforms. He has been active in the blockchain space since 2011 and has contributed to blockchain security research in conjunction with the University of South Florida's Department of Mathematics & Statistics."}
+                </p>
+                <p className="about-wrapper__info-text">
+                  {paragraphTwo ||
+                    "He has deep technical knowledge of blockchains, smart contracts, and oracles along with the ability to explain highly complex systems in an easily digestible format, as shown in his technical articles. His unique cultural background from the island of Curacao and his experience in the blockchain space makes him incredibly useful in this ever growing field of technology."}
+                </p>
+                {resume && (
+                  <span className="d-flex mt-3">
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="cta-btn cta-btn--resume"
+                      href={resume}
+                    >
+                      Resume
+                    </a>
+                  </span>
+                )}
+              </div>
+            </Fade>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default About;
